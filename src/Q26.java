85b64c5e2d4178b393a7f378ce642e199f17b3a1
佑仔 <a32871004@gmail.com>
2019-11-19 15:31:41 +0800

    Q26

--- src/Q26.java
+++ src/Q26.java
@@ -1,2 +1,14 @@
+import java.util.Scanner;
+
 public class Q26 {
+    public static void main(String[] args) {
+        Scanner sn=new Scanner(System.in);
+        int n=sn.nextInt();
+        int k=n;
+        while (n>=3){
+            k+=n/3;
+            n=n/3+n%3;
+        }
+        System.out.println(k);
+    }
 }
