9becda2b3cfbafe42246a095cf21bc47989af579
佑仔 <a32871004@gmail.com>
2019-11-19 15:31:13 +0800

    Q23

--- src/Q23.java
+++ src/Q23.java
@@ -1,2 +1,13 @@
+import java.util.Scanner;
+
 public class Q23 {
+    public static void main(String[] args) {
+        Scanner sn=new Scanner(System.in);
+        int n=sn.nextInt();
+        while (n>0){
+            System.out.print(n%10);
+            n/=10;
+        }
+        System.out.println();
+    }
 }
