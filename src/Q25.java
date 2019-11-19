bd41f990b1b52e36a82593c020c6ad4b68667621
佑仔 <a32871004@gmail.com>
2019-11-19 15:31:33 +0800

    Q25

--- src/Q25.java
+++ src/Q25.java
@@ -1,2 +1,31 @@
+import java.util.Scanner;
+
 public class Q25 {
+    public static void main(String[] args) {
+        Scanner sn=new Scanner(System.in);
+        int n=sn.nextInt();
+        int sum=0,j;
+        boolean flag=true;
+        for (int i=2;i<=n;i++){
+            j=i;
+            for (;j!=1;){
+                if (j%2==0){
+                    j/=2;
+                }
+                else if(j%3==0){
+                    j/=3;
+                }
+                else if (j%5==0){
+                    j/=5;
+                }
+                else {
+                    flag=false;
+                    break;
+                }
+            }
+            if(flag)sum+=i;
+            flag=true;
+        }
+        System.out.println(sum);
+    }
 }
