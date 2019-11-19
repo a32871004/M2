ec0d4c7002facbad7cd64b876057b2962668d1f7
佑仔 <a32871004@gmail.com>
2019-11-19 15:31:24 +0800

    Q24

--- src/Q24.java
+++ src/Q24.java
@@ -1,2 +1,16 @@
+import java.util.Scanner;
+
 public class Q24 {
+    public static void main(String[] args) {
+        Scanner sn = new Scanner(System.in);
+        int n = sn.nextInt();
+        int sum = 0;
+        for (int i=0;i<=n;i++){
+            sum+=i;
+            if (sum>n){
+                System.out.println(i-1);
+                break;
+            }
+        }
+    }
 }
